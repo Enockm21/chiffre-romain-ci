03a09164e46807c9880f99aa48269fcaa0cd113e
Enock MUKOKO MAYA <emukokomaya@gmail.com>
2023-01-27 23:43:55 +0100

    update ci-script file

--- ci-script.sh
+++ ci-script.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 
+# pour être sûr de faire de tester notre pull sur develop
+git checkout develop
 git pull
-#npm install
-#npm run build
+npm install
+npm run build
 npm test
 if [ $? -eq 0 ]; then
   echo "Tests passed"
+  git checkout main
+  git rebase develop master
  
 else
 
   current_date=$(date "+%F-%Hh-%Mm-%Ss")
   git branch "failures/$current_date"
-  #git reset HEAD~1
+  git reset HEAD~1
   echo "Test failed"
  # exit 1
 fi
