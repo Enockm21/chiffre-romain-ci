09ebd416eb980ca7035e29eb4a65a1dad7d062ff
Enock MUKOKO MAYA <emukokomaya@gmail.com>
2023-01-28 00:28:27 +0100

    Update script file

--- ci-script.sh
+++ ci-script.sh
@@ -1,23 +1,34 @@
 #!/usr/bin/env bash
 
-# pour être sûr de faire de tester notre pull sur develop
+# pour être sûr de tester notre pull sur develop
 git checkout develop
 git pull
 npm install
 npm run build
 npm test
 if [ $? -eq 0 ]; then
-  echo "Tests passed"
+
   git checkout main
   git rebase develop main
+  echo "Tests passed"
+
  
 else
-
+  #récupère la date pour créer la branche failure
   current_date=$(date "+%F-%Hh-%Mm-%Ss")
+
   git branch "failures/$current_date"
+
+  #publication de la nouvelle branche failure sur le repository  en remote (github/gitlab)
   git push --all -u
+
+  #suppression du dernier commit récupéré en pull 
   git reset HEAD~1
+
+  #suppression des modifications
   git restore .
+
+  #Mise à jour du repo 
   git push -f
   echo "Test failed"
   exit 1
